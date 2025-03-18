6d1e735ced8c4240c6ce9eab618746b5533e6dcf
Ivan Shargin <shargin.ia@phystech.edu>
2025-03-18 12:31:33 +0300

    rewrited tram mdp and polciy eval using dicts and sets

--- lessons/lesson_1/tram.py
+++ lessons/lesson_1/tram.py
@@ -5,6 +5,7 @@ import copy
 class TramMDP:
     def __init__(self, fail_prob, length=10, init_state=1, gamma=1):
         self.length = length
+        self.states = set(range(1, self.length+1))
         self.gamma = gamma
         assert self.length > 1, "length should be greater than 1"
         self.fail_prob = fail_prob
@@ -13,7 +14,10 @@ class TramMDP:
         self.utility = 0
     
     def get_actions(self):
-        pass
+        return self.actions
+    
+    def get_states(self):
+        return self.states
 
     def get_state(self):
         return self.state
@@ -24,8 +28,8 @@ class TramMDP:
         elif action == "tram":
             return [(min(2*state, self.length), 1 - self.fail_prob), (state, self.fail_prob)]
 
-    def is_terminal(self):
-        return self.state == self.length
+    def is_terminal(self, state):
+        return state == self.length
      
     def step(self, action):
         if action == "walk":
@@ -55,7 +59,7 @@ class PolicyEvaluation:
     def __init__(self, mdp, policy):
         self.mdp = mdp
         self.policy = policy
-        self.value = np.zeros(mdp.length)
+        self.values = {key: 0 for key in self.mdp.get_states()}
     
     def play_game(self):
         while not self.mdp.is_terminal():
@@ -63,46 +67,52 @@ class PolicyEvaluation:
             state, _ = self.mdp.step(action)
     
     def evaluate(self, num_iter):
-        for iter in range(num_iter):
-            new_values = np.zeros(self.mdp.length)
-            # print(f"old values {self.value}")
-            for s in range(1, self.mdp.length):
+        for iteration in range(num_iter):
+            new_values = {key: 0 for key in self.mdp.get_states()}
+            # print(f"old values {self.values}")
+            for s in self.mdp.get_states():
+                if self.mdp.is_terminal(s):
+                    continue
                 action = self.policy.act(s)
                 info = self.mdp.get_info(s, action)
                 for var in info:
                     next_s = var[0]
-                    # print(next_s)
                     prob = var[1]
                     if prob > 0:
-                        new_values[s-1] += prob * (-1 + self.value[next_s-1])
-                    # print(new_values)
-            # print(f'iter {iter}')
-            # print(new_values)
-            self.value = copy.deepcopy(new_values)
-        print(self.value)
-
+                        new_values[s] += prob * (-1. + self.values[next_s])
+            max_diff = max(abs(self.values[key] - new_values[key]) for key in self.values)
+            self.values = copy.deepcopy(new_values)
+            if max_diff < 1e-3:
+                print(f"Total number of iterations: {iteration}")
+                break
+        print(self.values)
 
 class DeterministicPolicy:
-    def __init__(self, actions_list):
-        self.actions_list = actions_list
+    def __init__(self, actions):
+        self.actions = actions
     
     def act(self, state):
-        return self.actions_list[state-1]
+        return self.actions[state]
 
 def play_game(mdp:TramMDP, policy):
     print(f"State = {mdp.get_state()}")
-    while not mdp.is_terminal():
+    while not mdp.is_terminal(mdp.get_state()):
         action = policy.act(mdp.get_state())
         print(action)
         state, _ = mdp.step(action)
         print(f"State = {mdp.get_state()}")
     print(f"Terminal state. Utility = {mdp.utility}")
 
-
 def main(args=None):
     mdp = TramMDP(0.3)
     # policy = SimplestPolicy("walk")
-    policy = DeterministicPolicy(["walk"] + ["tram"] * 9)
+    actions = dict()
+    for i in range(1, 11):
+        if i == 1:
+            actions[i] = "walk"
+        else:
+            actions[i] = "tram"
+    policy = DeterministicPolicy(actions)
     # play_game(mdp, policy)
     evaluator = PolicyEvaluation(mdp, policy)
     evaluator.evaluate(1000)
